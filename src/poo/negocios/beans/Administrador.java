d138b11225627f066f0045a351ea1336a9aa52ca
thiago123789 <thiago123789@gmail.com>
2016-04-15 13:13:52 -0300

    1.2

--- src/poo/negocios/beans/Administrador.java
+++ src/poo/negocios/beans/Administrador.java
@@ -7,24 +7,24 @@ import poo.excecoes.PermissaoInvalidaException;
 import poo.negocios.beans.davi.*;
 import poo.negocios.beans.sidronio.*;
 
-public class Administrador extends Pessoa{
-    private int permissao;
-       
-    public Administrador(String pNome, String uNome, String cpf, int permissao) throws CPFInvalidoException, PermissaoInvalidaException{
-        super(pNome, uNome, cpf);
-        this.setPermissao(permissao);
-    }
-    
-	public int getPermissao() {
-		return permissao;
-	}
-
-	public void setPermissao(int persmissao) throws PermissaoInvalidaException{
-        if(permissao >= 0 && permissao < 3){
-            this.permissao = permissao;
-        }else{
-            throw new PermissaoInvalidaException();
-        }
-    }
+public class Administrador {
+//    private int permissao;
+//       
+//    public Administrador(String pNome, String uNome, String cpf, int permissao) throws CPFInvalidoException, PermissaoInvalidaException{
+//        super(pNome, uNome, cpf);
+//        this.setPermissao(permissao);
+//    }
+//    
+//	public int getPermissao() {
+//		return permissao;
+//	}
+//
+//	public void setPermissao(int persmissao) throws PermissaoInvalidaException{
+//        if(permissao >= 0 && permissao < 3){
+//            this.permissao = permissao;
+//        }else{
+//            throw new PermissaoInvalidaException();
+//        }
+//    }
    
 }
