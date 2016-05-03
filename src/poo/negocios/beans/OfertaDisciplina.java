ef7968907af585d9e64155d47126f3fbfd703f06
Davi <davi.gomes.sales@gmail.com>
2016-05-03 20:06:47 -0300

    Classe Oferta excluida, atributos transferidos para OfertaDisciplina, Criação das classes EmpresaJunior e Localização.

--- src/poo/negocios/beans/OfertaDisciplina.java
+++ src/poo/negocios/beans/OfertaDisciplina.java
@@ -1,5 +1,11 @@
 package poo.negocios.beans;
 
+import java.util.ArrayList;
+
 public class OfertaDisciplina {
-	
+	private Disciplina disciplina;
+	private int ano, semestre;
+	private ArrayList<Horario> horario;
+	private int id;
+	private String turma;
 }
